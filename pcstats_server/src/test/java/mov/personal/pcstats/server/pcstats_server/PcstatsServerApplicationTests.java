c8edb9e880c8f116f3c138a7d2e75b91e79ead23
Vlad <vlad.mocanu.alexandru@gmail.com>
2020-10-22 18:28:31 +0300

    Added basic project files for server and client

---
+++ pcstats_server/src/test/java/mov/personal/pcstats/server/pcstats_server/PcstatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mov.personal.pcstats.server.pcstats_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcstatsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
