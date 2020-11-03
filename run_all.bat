854d5cd1e033fbe60553bb598c479da20de4f71d
Vlad Mocanu <vlad.mocanu.alexandru@gmail.com>
2020-11-03 18:21:21 +0200

    Improved run_all batch script to also build mvn projects

--- run_all.bat
+++ run_all.bat
@@ -1,2 +1,3 @@
+cmd /C "mvn clean install"
 start "aggregator" cmd /C "cd aggregator/target & java -jar aggregator-1.0.jar"
 start "html_client" cmd /C "cd client_html/target & java -jar client_html-1.0.jar"
