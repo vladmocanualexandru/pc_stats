7035c22c9fb63e6f1e7647a54a73b08e292853cf
Vlad@Aqirys <vlad.mocanu.alexandru@gmail.com>
2022-11-29 00:43:11 +0200

    Updated code to allow shutdown under rasbian

--- mqtt_command_listener.py
+++ mqtt_command_listener.py
@@ -1,15 +1,9 @@
 import paho.mqtt.client as mqtt
-import json, subprocess
-
-MQTT_BROKER = "home.hub"
-MQTT_PORT = 1883
-MQTT_USER = "pc_stats_rpi"
-MQTT_PASSWORD = "FeW*!!XQgb%duykLcHqW$q3vtQ&%@jh7R9noHQ!QNTX#P^w!fCLdi%2$Z4TFoSUHy!E@?ogo$X9H@rfbPheK!eQ^pW%XK3sLeACqjmN%&x4kb?$@RaHVk$^FJPhCF4Ps"
-MQTT_TOPIC = "hass-out/control-pc-stats-rpi"
+import json, os
 
 def handle_command(command):
     if command == 'shutdown':
-        subprocess.run(["shutdown"])
+        os.system("sudo shutdown now -h")
         exit(1)
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -18,7 +12,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(MQTT_TOPIC)
+    client.subscribe(os.getenv('MQTT_TOPIC'))
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -30,9 +24,9 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+client.username_pw_set(os.getenv('MQTT_USER'), os.getenv('MQTT_PASSWORD'))
 
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
+client.connect(os.getenv('MQTT_BROKER'), int(os.getenv('MQTT_PORT')), 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
